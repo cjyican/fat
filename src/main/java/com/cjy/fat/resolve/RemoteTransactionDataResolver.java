5e777e66c57e8acb42af993cd19ba919086fac99
Adminis <Adminis@ican>
2019-12-30 10:11:46 +0800

    优化注入远程事务数据接收器逻辑

--- src/main/java/com/cjy/fat/resolve/RemoteTransactionDataResolver.java
+++ src/main/java/com/cjy/fat/resolve/RemoteTransactionDataResolver.java
@@ -1,9 +1,8 @@
 package com.cjy.fat.resolve;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import com.cjy.fat.data.TransactionContent;
@@ -12,10 +11,8 @@ import com.cjy.fat.resolve.accept.RemoteTransactionAccepter;
 @Component
 public class RemoteTransactionDataResolver {
 	
-	private static final Logger LOG = LoggerFactory.getLogger(RemoteTransactionDataResolver.class);
-	
 	@Autowired
-	ApplicationContext context;
+	List<RemoteTransactionAccepter> accepters;
 	
 	/**
 	 * 初始化远程事务信息
@@ -23,11 +20,16 @@ public class RemoteTransactionDataResolver {
 	public void init() {
 		//初始化当前容器
 		TransactionContent.initContainer();
-		String[] accepterNames = context.getBeanNamesForType(RemoteTransactionAccepter.class);
-		if(accepterNames != null && accepterNames.length > 0) {
-			for(int i = 0 ; i < accepterNames.length ; i++) {
-				LOG.info("load {} " , accepterNames[i]);
-				RemoteTransactionAccepter accepter = (RemoteTransactionAccepter) context.getBean(accepterNames[i]);
+//		String[] accepterNames = context.getBeanNamesForType(RemoteTransactionAccepter.class);
+//		if(accepterNames != null && accepterNames.length > 0) {
+//			for(int i = 0 ; i < accepterNames.length ; i++) {
+//				LOG.info("load {} " , accepterNames[i]);
+//				RemoteTransactionAccepter accepter = (RemoteTransactionAccepter) context.getBean(accepterNames[i]);
+//				accepter.acceptRemoteTransactionData();
+//			}
+//		}
+		if(accepters != null && !accepters.isEmpty()) {
+			for(RemoteTransactionAccepter accepter : accepters) {
 				accepter.acceptRemoteTransactionData();
 			}
 		}
